44efd85b3bfb5f1615ca5b5527d8af02d146c7e3
marcusfgardiner <marcusfgardiner@gmail.com>
2018-04-19 14:32:32 +0100

    feature: storing note input into form in a session

--- app.rb
+++ app.rb
@@ -1,13 +1,20 @@
 require 'sinatra/base'
 
 class Notes < Sinatra::Base
+  enable :sessions
 
-  get '/' do
-    redirect '/notes'
-  end
+get '/' do
+  redirect '/notes'
+end
 
 get '/notes' do
+  p session[:note]
   erb :index
+end
+
+post '/notes' do
+  session[:note] = params['note']
+  redirect '/notes'
 end
 
 end
